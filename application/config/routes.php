0754f90ee79bcfd45c49be9c1e0367976b92d3da
Dana Sugu <dana.sugu@gmail.com>
2021-10-20 11:02:30 +0300

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,7 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
  
-$route['default_controller'] = 'welcome';
-$route['products'] = 'index';
+$route['default_controller'] = 'pages/view';
+$route['products/index'] = 'products/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['(:any)'] = 'pages/view/$1';
